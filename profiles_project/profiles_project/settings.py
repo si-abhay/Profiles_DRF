ee5eebc9243397d820aad3fcc4d8c41380614e32
Abhay <asingh20_be20@thapar.edu>
2024-02-02 13:52:19 +0530

    abstract user base manager

--- profiles_project/profiles_project/settings.py
+++ profiles_project/profiles_project/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+# Custom user model
+AUTH_USER_MODEL = 'profiles_api.UserProfile'
